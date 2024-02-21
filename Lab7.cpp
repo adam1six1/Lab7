a592c4599814930d1e128a42501ea8c52de2d6a5
adam1six1 <adam1six1@gmail.com>
2024-02-21 11:39:06 -0500

    pasted template code

--- Lab7.cpp
+++ Lab7.cpp
@@ -1,20 +1,44 @@
-// Lab7.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
-
+#include <SFML/Graphics.hpp>
+#include <SFPhysics.h>
+using namespace std;
+using namespace sf;
+using namespace sfp;
 int main()
 {
-    std::cout << "Hello World!\n";
+	// Create our window and world with gravity 0,1
+	RenderWindow window(VideoMode(800, 600), "Bounce");
+	World world(Vector2f(0, 1));
+	// Create the ball
+	PhysicsCircle ball;
+	ball.setCenter(Vector2f(400, 300));
+	ball.setRadius(20);
+	world.AddPhysicsBody(ball);
+	// Create the floor
+	PhysicsRectangle floor;
+	floor.setSize(Vector2f(800, 20));
+	floor.setCenter(Vector2f(400, 590));
+	floor.setStatic(true);
+	world.AddPhysicsBody(floor);
+	int thudCount(0);
+	floor.onCollision = [&thudCount](PhysicsBodyCollisionResult result) {
+		cout << "thud " << thudCount << endl;
+		thudCount++;
+		};
+	Clock clock;
+	Time lastTime(clock.getElapsedTime());
+	while (true) {
+		// calculate MS since last frame
+		Time currentTime(clock.getElapsedTime());
+		Time deltaTime(currentTime - lastTime);
+		int deltaTimeMS(deltaTime.asMilliseconds());
+		if (deltaTimeMS > 0) {
+			world.UpdatePhysics(deltaTimeMS);
+			lastTime = currentTime;
+		}
+		window.clear(Color(0, 0, 0));
+		window.draw(ball);
+		window.draw(floor);
+		window.display();
+	}
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
