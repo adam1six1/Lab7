7982372aeac93562814dc92b4f98059a7b423949
adam1six1 <adam1six1@gmail.com>
2024-02-21 11:51:28 -0500

    added walls and celing

--- Lab7.cpp
+++ Lab7.cpp
@@ -20,6 +20,28 @@ int main()
 	floor.setCenter(Vector2f(400, 590));
 	floor.setStatic(true);
 	world.AddPhysicsBody(floor);
+
+	//Create Left wall
+	PhysicsRectangle wallLeft;
+	wallLeft.setSize(Vector2f(20, 600));
+	wallLeft.setCenter(Vector2f(10, 300));
+	wallLeft.setStatic(true);
+	world.AddPhysicsBody(wallLeft);
+
+	//Create Right wall
+	PhysicsRectangle wallRight;
+	wallRight.setSize(Vector2f(20, 600));
+	wallRight.setCenter(Vector2f(790, 300));
+	wallRight.setStatic(true);
+	world.AddPhysicsBody(wallRight);
+
+	//Create Celing
+	PhysicsRectangle celing;
+	celing.setSize(Vector2f(800, 20));
+	celing.setCenter(Vector2f(400, 10));
+	celing.setStatic(true);
+	world.AddPhysicsBody(celing);
+
 	int thudCount(0);
 	floor.onCollision = [&thudCount](PhysicsBodyCollisionResult result) {
 		cout << "thud " << thudCount << endl;
@@ -39,6 +61,12 @@ int main()
 		window.clear(Color(0, 0, 0));
 		window.draw(ball);
 		window.draw(floor);
+
+		//Draw Walls & Celing
+		window.draw(wallLeft);
+		window.draw(wallRight);
+		window.draw(celing);
+
 		window.display();
 	}
 }
